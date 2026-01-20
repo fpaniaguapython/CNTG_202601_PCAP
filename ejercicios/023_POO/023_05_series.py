6decdabf7cf243ab43ce6ca890d198090a492353
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-01-20 17:31:28 +0100

    Create 023_05_series.py

---
+++ ejercicios/023_POO/023_05_series.py
@@ -0,0 +1,14 @@
+# La clase Participante tiene:
+# nombre, nacionalidad, nombre del papel
+class Participante:
+    pass
+
+# La clase Serie tiene:
+# título, género, número de temporadas, plataforma, participantes
+class Serie:
+    pass
+
+# Modelar las clases
+# Crear un objeto serie que tenga al menos 3 participantes
+# Mostrar toda la información de una serie (todos los atributos de Serie) y
+# todos lode Participante
